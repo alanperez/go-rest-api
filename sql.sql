63a708b1b040ab82331f375ee1fa7020e174f12b
Alan Perez <alanp9six@gmail.com>
2019-08-07 23:39:02 -0500

    :memo: planning how the table would look

--- sql.sql
+++ sql.sql
@@ -1,9 +1,10 @@
 create database droom;
-
+-- creates table named droom
 \c droom;
 
+-- table struct
 create table if not exists users (
-  id bigserial primary key,
+  id serial primary key,
   email varchar(40) not null unique,
   password varchar(100) not null,
   status char(1) default '0'
@@ -11,6 +12,7 @@ create table if not exists users (
   updated_at timestamp default current_timestamp
 );
 
+-- might need a seperate table.
 create table if not exists companies (
 
 )
